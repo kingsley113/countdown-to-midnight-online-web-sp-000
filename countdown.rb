5d8c3b1449c547cc3d92d7bc680d9509fb8ace4c
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-07-10 05:02:57 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -11,3 +11,16 @@ end
 
 
 countdown(10)
+
+
+def countdown_with_sleep(number)
+  while number > 0
+    puts "#{number} SECONDS!"
+    number -= 1
+    sleep(1)
+  end
+
+  puts "HAPPY NEW YEAR!"
+end
+
+countdown_with_sleep(5)
